c80d01748bab63d12083a730da10ed2e8ec25254
shobhitsrivastava <shobhit@gatech.edu>
2016-09-27 15:42:23 -0400

    added User class in Model

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,7 +1,26 @@
 package Model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Created by DudeLong on 2016/09/20.
  */
 public class User {
+    private String username = "user";
+    private String password = "password";
+
+    public String get_username() {
+        return username;
+    }
+
+
+    public String get_password() {
+        return password;
+    }
+
+    public void setUserName(String username) {this.username = username; }
+
+    public void setPassword(String password) {this.password = password;}
+
 }
