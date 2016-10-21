ae3659c0fee0593dcd4a3417d8060c930cfd1726
shobhitsrivastava <shobhit@gatech.edu>
2016-10-20 20:28:09 -0400

    added web controller

--- src/Controller/AvailabilityScreenController.java
+++ src/Controller/AvailabilityScreenController.java
@@ -8,11 +8,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+
 
 /**
  * A controller for the availability screen
@@ -22,8 +28,19 @@ import java.util.logging.Logger;
  * @version 1.1
  */
 public class AvailabilityScreenController {
-
+    @FXML
     private Stage availabilityScreenStage;
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private WebView webView;
+
+    private WebEngine webEngine;
+
+    //WebEngine engine = webView.getEngine();
+    //engine.load();
 
     private boolean _okClicked = false;
 
@@ -32,6 +49,8 @@ public class AvailabilityScreenController {
 
     @FXML
     private void initialize() {
+        webEngine = webView.getEngine();
+        //webEngine.load(MainFXApplication.class.getResource("../Food fight/index.html").toString());
     }
 
     /**
@@ -50,14 +69,12 @@ public class AvailabilityScreenController {
      */
     public void setAvailabilityScreenStage(Stage newStage) {
         availabilityScreenStage = newStage;
+        availabilityScreenStage.setTitle("Water Reports Map");
     }
 
     public boolean isOkClicked() {
         return _okClicked;
     }
-
-
-
 
 
 }
