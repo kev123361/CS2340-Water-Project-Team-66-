9170ffdd1b8a3a053b5102bec3eda01dbeca404b
shobhitsrivastava <shobhit@gatech.edu>
2016-10-26 00:55:11 -0400

    added purity reports to model

---
+++ src/Model/PurityReport.java
@@ -0,0 +1,80 @@
+package Model;
+
+import javafx.beans.property.*;
+
+/**
+ * Created by 2shob on 10/25/2016.
+ */
+public class PurityReport {
+    private final StringProperty _date = new SimpleStringProperty();
+    private final StringProperty _time = new SimpleStringProperty();
+    private final IntegerProperty _reportNum = new SimpleIntegerProperty();
+    private static int reportNumCounter = 0;
+    private final ObjectProperty<User> _reportingUser = new SimpleObjectProperty<>();
+    private final DoubleProperty _latitude = new SimpleDoubleProperty();
+    private final DoubleProperty _longitude = new SimpleDoubleProperty();
+    private final ObjectProperty<OverallConditionOfWater> _overallWaterCondition = new SimpleObjectProperty<>();
+    private final IntegerProperty _virusPPM = new SimpleIntegerProperty();
+    private final IntegerProperty _contaminantPPM = new SimpleIntegerProperty();
+
+    public PurityReport() {
+        this(null, null, null, 0.0, 0.0, null, 0, 0);
+    }
+
+    public PurityReport(String date, String time, User reportingUser, Double latitude,
+                        Double longitude, OverallConditionOfWater overallWaterCondition, int virusPPM,
+                        int contaminantPPM) {
+        _date.set(date);
+        _time.set(time);
+        _reportNum.set(reportNumCounter);
+        reportNumCounter++;
+        _reportingUser.set(reportingUser);
+        _latitude.set(latitude);
+        _longitude.set(longitude);
+        _overallWaterCondition.set(overallWaterCondition);
+        _virusPPM.set(virusPPM);
+        _contaminantPPM.set(contaminantPPM);
+    }
+
+    public String getDate() { return _date.get(); }
+    public void setDate(String newDate) { _date.set(newDate); }
+
+    public String getTime() { return _time.get(); }
+    public void setTime(String newTime) { _time.set(newTime); }
+
+    public User getReportingUser() { return _reportingUser.get(); }
+    public void setReportingUser(User newUser) { _reportingUser.set(newUser); }
+
+    public int getReportNum() { return _reportNum.get(); }
+    public void setReportNum(int newReportNum) { _reportNum.set(newReportNum); }
+
+    public double getLatitude() { return _latitude.get(); }
+    public void setLatitude(double newLat) { _latitude.set(newLat); }
+
+    public double getLongitude() { return _longitude.get(); }
+    public void setLongitude(double newLong) { _longitude.set(newLong); }
+
+    public OverallConditionOfWater getOverallWaterCondition() {
+        return _overallWaterCondition.get();
+    }
+    public void setOverallWaterCondition(OverallConditionOfWater overallWaterCondition) {
+        _overallWaterCondition.set(overallWaterCondition);
+    }
+
+    public int getVirusPPM() {
+        return _virusPPM.get();
+    }
+    public void setVirusPPM(int ppm) {
+        _virusPPM.set(ppm);
+    }
+
+    public int getContaminantPPM() {
+        return _contaminantPPM.get();
+    }
+    public void setContaminantPPM(int ppm) {
+        _contaminantPPM.set(ppm);
+    }
+
+
+
+}
